e84a22a129baddce00d722dfd12805d8fde0a803
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2024-11-25 08:44:42 +0300

    [server.db] Define tables

--- server/db.py
+++ server/db.py
@@ -1,56 +1,119 @@
+from datetime import date
 from logging import getLogger
 from typing import final
 
-from psycopg import AsyncConnection
-from psycopg.conninfo import conninfo_to_dict
+from sqlalchemy import (
+    BigInteger,
+    Date,
+    Double,
+    ForeignKey,
+    PrimaryKeyConstraint,
+    String,
+    select,
+    )
+from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+from server.models import CargoType, RawCargoType, Tariff
 
 logger = getLogger(__name__)
 
 
-async def configure_connection(conn: AsyncConnection, /) -> None:
+class BaseTable(DeclarativeBase):
     """
-    A function for configuring a database connection.
+    Base class for all database models.
     """
-    await conn.set_autocommit(True)
-    await conn.set_read_only(True)
 
 
-async def verify_connectivity(uri: str, /) -> bool:
-    """
-    Verifies whether a connection to the database with the given URI can be established
-    and logs errors if it is not possible.
-    """
-    try:
-        parsed = conninfo_to_dict(uri)
-        logger.info(
-            f"Verifying connectivity to the database "
-            f"postgresql://{parsed['user']}:REDACTED@"
-            f"{parsed['host']}:{parsed['port']}/{parsed['dbname']}"
-            )
-    except Exception:
-        logger.exception('Failed to parse the provided PostgreSQL URI')
-        return False
+class CargoTypeTable(BaseTable):
+    __tablename__ = 'cargo_types'
 
-    try:
-        async with await AsyncConnection.connect(uri) as conn:
-            await configure_connection(conn)
-            logger.info('Connection to the database can be established successfully')
-    except Exception:
-        logger.exception('Connection to the database cannot be established')
-        return False
+    id: Mapped[int] = mapped_column(BigInteger, autoincrement=True, primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
 
-    return True
+
+class TariffTable(BaseTable):
+    __tablename__ = 'tariffs'
+
+    date: Mapped[date] = mapped_column(Date, nullable=False)
+    cargo_type: Mapped[int] = mapped_column(
+        ForeignKey(CargoTypeTable.id, ondelete='CASCADE'),
+        nullable=False,
+        )
+    rate: Mapped[float] = mapped_column(Double, nullable=False)
+
+    __table_args__ = (
+        PrimaryKeyConstraint(date, cargo_type, name='unique_date_cargo_type'),
+        )
 
 
 @final
-class PostgreSQLRequester:
+class DatabaseRequester:
     """
-    A class for querying PostgreSQL database.
+    A class for querying the database.
     """
-    __slots__ = '_conn',
+    __slots__ = '_session',
 
-    def __init__(self, connection: AsyncConnection, /) -> None:
-        self._conn = connection
+    def __init__(self, session: AsyncSession, /) -> None:
+        self._session = session
+
+    async def fetch_cargo_type(self, cargo_type_name: RawCargoType, /) -> CargoType | None:
+        """
+        Fetches ID for the given cargo type name,
+        then returns :class:`CargoType` or ``None`` if no such cargo type exists.
+        """
+        query = select(CargoTypeTable).where(CargoTypeTable.name == cargo_type_name)
+        result = await self._session.scalars(query)
+        row = await result.first()
+        return CargoType.model_validate(row) if row else None
+
+    async def add_cargo_types(
+            self,
+            cargo_type_name: RawCargoType,
+            /,
+            *cargo_type_names: RawCargoType,
+            ) -> list[CargoType]:
+        """
+        Adds specified cargo types to the database if they are not present.
+        Returns the list of added cargo types.
+        """
+        vals = [dict(name=cargo_type_name)]
+        vals.extend(dict(name=name) for name in cargo_type_names)
+
+        query = (
+            insert(CargoTypeTable)
+            .values(vals)
+            .on_conflict_do_nothing()
+            .returning(CargoTypeTable)
+        )
+        result = await self._session.scalars(query)
+        li = [CargoType.model_validate(row) for row in result]
+        for row in li:
+            logger.info(f'Added cargo type {row}')
+            # todo log all added rows to kafka
+
+        return li
+
+    async def fetch_tariff(
+            self,
+            ensurance_date: date,
+            cargo_type: CargoType,
+            /
+            ) -> Tariff | None:
+        """
+        Fetches tariff for the given date and cargo type,
+        then returns :class:`Tariff` or ``None`` if no such tariff exists.
+        """
+        query = (
+            select(TariffTable)
+            .where(TariffTable.date == ensurance_date)
+            .where(TariffTable.cargo_type == cargo_type.id)
+
+        )
+        result = await self._session.scalars(query)
+        row = await result.first()
+        return Tariff(date=ensurance_date, cargo_type=cargo_type, rate=row.rate) if row else None
 
 
-__all__ = 'PostgreSQLRequester', 'configure_connection', 'verify_connectivity'
+__all__ = 'BaseTable', 'DatabaseRequester'
