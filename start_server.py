f1256922aa5ddadff9369becf541828bf2eab2c0
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2024-11-25 23:18:18 +0300

    Add script for starting server locally

---
+++ start_server.py
@@ -0,0 +1,35 @@
+if __name__ == '__main__':
+    import os
+    import sys
+
+    from api import app
+
+    os.chdir(os.path.dirname(__file__))
+
+    params = dict(
+        app=app,
+        env_file='.env',
+        port=2127,
+        log_config='server/logging-config.json',
+        )
+
+    try:
+        # If on Windows, set AsyncIO loop policy compatible with Psycopg
+        if sys.platform == 'win32':
+            import asyncio
+
+            from uvicorn import Config, Server
+
+            asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+
+            config = Config(**params, loop='asyncio')
+            server = Server(config)
+            loop.run_until_complete(server.serve())
+        else:
+            from uvicorn import run
+
+            run(**params)
+    except KeyboardInterrupt:
+        pass
